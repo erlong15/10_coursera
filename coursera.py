bb7ce583af4ba17ebf9fdcca93754a7a9395bd22
Alex <lucky_is@mail.ru>
2017-10-04 21:32:28 +0300

    notices have been fixed

--- coursera.py
+++ coursera.py
@@ -6,12 +6,16 @@ from lxml import html
 from openpyxl import Workbook
 
 
-def get_course_info(course_uri):
+def get_course_content(course_uri):
     course_req = requests.get(course_uri)
-    if course_req.status_code != 200:
-        return {}
+    if not course_req.ok:
+        return None
+    else:
+        return html.fromstring(course_req.content)
 
-    tree = html.fromstring(course_req.content)
+
+def get_course_info(content, course_uri):
+    tree = content
     title = tree.xpath('//div[contains(@class,"header-container")]/h1[contains(@class,"title")]')
     start_date = tree.xpath('//div[contains(@class,"startdate")]/span')
     basic_info = tree.xpath('//div[@class="rc-BasicInfo"]/table/tbody/tr')
@@ -36,37 +40,59 @@ def get_course_info(course_uri):
 
 
 def get_courses_list(xml_uri, courses_count):
-    request = requests.get(xml_uri)
-    soup = BeautifulSoup(request.content, 'lxml')
-    courses = map(lambda x: get_course_info(x.get_text()),
-                  random.sample(soup.find_all('loc'),
-                  courses_count))
-    return courses
+    response = requests.get(xml_uri)
+    soup = BeautifulSoup(response.content, 'lxml')
+    rand_courses = random.sample(soup.find_all('loc'), courses_count)
+    course_urls = map(lambda x: x.get_text(), rand_courses)
+
+    return course_urls
 
 
-def output_courses_info_to_xlsx(courses, excel_name):
+def format_output_to_xlsx(courses):
+    output_array = []
+    for course in courses:
+        output_array.append([course['title'],
+                             course['start_date'],
+                             course['language'],
+                             course['commitment'],
+                             course['rating'],
+                             course['uri']])
+    return output_array
+
+
+def save_xlsx(formatted_courses, excel_name):
     wb = Workbook(write_only=True)
     ws = wb.create_sheet()
-
-    for course in courses:
-        ws.append([course['title'],
-                   course['start_date'],
-                   course['language'],
-                   course['commitment'],
-                   course['rating'],
-                   course['uri']
-                   ]
-                  )
+    for course_row in formatted_courses:
+        ws.append(course_row)
     wb.save(excel_name)
 
 
-if __name__ == '__main__':
-    xml_uri = 'https://www.coursera.org/sitemap~www~courses.xml'
+def get_args():
     parser = argparse.ArgumentParser(description='this is a coursera random courses grabber.')
-    parser.add_argument('-c', '--count', help='How much courses do you want to scrape (default: 5)', required=False)
-    parser.add_argument('-o', '--output', help='Output excel file name (default: courses.xlsx)', required=False)
+    parser.add_argument('-c', '--count',
+                        help='How much courses do you want to scrape (default: 5)',
+                        required=False,
+                        default=5)
+    parser.add_argument('-o', '--output',
+                        help='Output excel file name (default: courses.xlsx)',
+                        required=False,
+                        default='courses.xlsx')
     args = parser.parse_args()
-    courses_count = args.count or 5
-    filename = args.output or 'courses.xlsx'
-    courses = get_courses_list(xml_uri, courses_count)
-    output_courses_info_to_xlsx(courses, filename)
+    return args
+
+
+def process_all_courses(courses_count):
+    xml_uri = 'https://www.coursera.org/sitemap~www~courses.xml'
+    courses = []
+    for course_url in get_courses_list(xml_uri, courses_count):
+        content = get_course_content(course_url)
+        if content is not None:
+            courses.append(get_course_info(content, course_url))
+    return courses
+
+
+if __name__ == '__main__':
+    args = get_args()
+    course_list = process_all_courses(args.count)
+    save_xlsx(format_output_to_xlsx(course_list), args.output)
